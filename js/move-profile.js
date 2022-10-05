f1a84364543e79c39177940af04c3197611795ff
sunnao <osa125125@naver.com>
2022-10-06 02:48:32 +0900

    말풍선 수정 말풍선 애니메이션, 내용 추가

--- js/move-profile.js
+++ js/move-profile.js
@@ -1,13 +1,13 @@
 const movingImg = document.querySelector("#profileImg");
 const cameraFrame = document.querySelector("#cameraFrame");
 const frame = document.querySelector(".frameDiv");
+const bubbleArea = document.querySelector("#bubbleArea");
 
 function takePhoto(){
     cameraFrame.animate({
-                    opacity: [0, 1 ]}, 
+                    opacity: [0, 1]}, 
                     {
                     duration : 500,
-                    easing: "ease",
                     fill:"forwards",});
 }
 function closer(){
@@ -18,18 +18,23 @@ function closer(){
                     duration: 1000, 
                     easing: 'ease',
                     fill: "forwards",
-                    delay: 300})
+                    delay: 300});
 }
-
-
+function bubbleShow(){
+    bubbleArea.animate({
+                    opacity: [0, 1]}, 
+                    {
+                    duration : 300,
+                    easing: "ease",
+                    fill:"forwards",
+                    delay: 500});
+}
 function takeoffPhoto(){
     cameraFrame.animate({
         opacity: [1, 0 ]}, 
         {
-        duration : 500,
-        fill:"forwards",
-        easing: "ease",
-        fill: "forwards"});
+        duration : 100,
+        fill:"forwards"});
 }
 function faraway() {
     movingImg.animate ({
@@ -41,8 +46,17 @@ function faraway() {
             fill: "forwards",
             delay: 100})
 }
+function bubbleOff(){
+    bubbleArea.animate({
+                    opacity: [1, 0]}, 
+                    {
+                    duration : 100,
+                    fill:"forwards"});
+}
 frame.addEventListener("mouseover",takePhoto)
 frame.addEventListener("mouseover",closer)
+frame.addEventListener("mouseover",bubbleShow)
 frame.addEventListener("mouseout",takeoffPhoto)
 frame.addEventListener("mouseout",faraway)
+frame.addEventListener("mouseout",bubbleOff)
 
